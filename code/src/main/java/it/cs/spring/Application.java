795fb11cb7b901f7b826d9832b85704b2157bf13
giulianobana <41050102+giulianobana@users.noreply.github.com>
2018-11-28 11:54:45 +0100

    move to wild fly

--- code/src/main/java/it/cs/spring/Application.java
+++ code/src/main/java/it/cs/spring/Application.java
@@ -3,11 +3,21 @@ package it.cs.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
  
     public static void main(String[] args) {
-    	SpringApplication.run(Application.class, args);
+        SpringApplication.run(applicationClass, args);
     }
+ 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+ 
+    private static Class<Application> applicationClass = Application.class;
 }
